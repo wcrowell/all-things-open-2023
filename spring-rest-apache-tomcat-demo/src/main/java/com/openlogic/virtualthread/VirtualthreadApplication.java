9b8b3eda7165af189ab3e341be91b5b785693947
wcrowell <wcrowell@perforce.com>
2023-10-16 11:18:58 -0400

    Add files via upload
    Initial check-in

---
+++ spring-rest-apache-tomcat-demo/src/main/java/com/openlogic/virtualthread/VirtualthreadApplication.java
@@ -0,0 +1,22 @@
+package com.openlogic.virtualthread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+public class VirtualthreadApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualthreadApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(VirtualthreadApplication.class);
+    }
+}
