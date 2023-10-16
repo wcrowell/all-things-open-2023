577c50cfef352a5956061539ce2688898b40fe91
wcrowell <wcrowell@perforce.com>
2023-10-16 11:53:29 -0400

    Add files via upload

---
+++ spring-rest-apache-tomcat-demo/spring-rest-apache-tomcat-demo/src/main/java/com/openlogic/rest/Product.java
@@ -0,0 +1,43 @@
+package com.openlogic.rest;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	private String name;
+    private Long price;
+    
+    public Long getId() {
+		return id;
+	}
+	
+    public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Long getPrice() {
+		return price;
+	}
+
+	public void setPrice(Long price) {
+		this.price = price;
+	}
+}
---
+++ spring-rest-apache-tomcat-demo/spring-rest-apache-tomcat-demo/src/main/java/com/openlogic/rest/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.openlogic.rest;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
